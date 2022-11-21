3bb609cee8b74ff81610f6e3418931aa3c0b883d
sdfields <spencerdfields@gmail.com>
2022-11-20 20:56:48 -0600

    added dayjs to reflect current date at the top

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+var now = dayjs().format("MMMM D, YYYY")
+$("#currentDay").text(now)
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
